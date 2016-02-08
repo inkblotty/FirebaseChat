ed774abced8f1590da0db7ae209c87eee4929115
inkblotty <inkblot.and.blush@gmail.com>
2016-02-08 15:39:14 -0700

    renamed index.js to server.js; rewrote static server so that external css can be used

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ var path = require('path');
 var port = 3000;
 var http = require('http').Server(app);
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 http.listen(3000, function() {
 	console.log('listening on *:3000');
